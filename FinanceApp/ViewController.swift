66f7a711e00401a722e99c5d5954077106d1b87f
jjb_dev <84111581+jjbuell-dev@users.noreply.github.com>
2023-01-19 22:22:52 -0700

    Initial Commit

---
+++ FinanceApp/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//
+// FinanceApp
+// 
+// ViewController.swift
+//
+// Created by Royal Blue Software
+// 
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .green
+    }
+
+
+}
+
